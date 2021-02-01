72c862987e73890c6b4b36d91609c589f74cb812
murilo.preccaro <murilo.preccaro@acct.global>
2021-02-01 13:57:08 -0300

    added lint and ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Twitter Prediction</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Link href="/login">
-                <a>Login to Twitter</a>
-
-                
-            </Link>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Twitter Prediction</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Link href="/login">
+        <a>Login to Twitter</a>
+      </Link>
+    </div>
+  );
 }
